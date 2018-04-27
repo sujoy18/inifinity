5d9493bcb657c8255e916de7c12b4f4a350fc3cb
Ifthekhar <sujoy18@yahoo.com>
2018-04-27 17:31:57 +1000

    first commit

---
+++ server/server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+console.log(__dirname);
+app.use('/', express.static( './build')); //serves the index.html
+app.listen(3000,'localhost',function(){
+    console.log('listens on port 3000 -> http://localhost:3000/')
+}); //listens on port 3000 -> http://localhost:3000/
